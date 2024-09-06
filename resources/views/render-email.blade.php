838b680dbbe13c4f4984b25ba2c74cc8c6f38359
BackanForth <63647721+CoreI0@users.noreply.github.com>
2024-09-06 15:35:40 +0200

    Delete resources directory

--- resources/views/render-email.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach(array_keys($senderInfo) as $key)
-	@foreach(array_keys($senderInfo[$key]) as $key2)
-		@if($key2!="id")
-			{{$key2}} : {{$senderInfo[$key][$key2]}}
-			<br/>
-		@endif
-	@endforeach
-@endforeach
